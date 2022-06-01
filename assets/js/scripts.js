16272813d0486914d2aa78216e3e04f105a218a3
guilhermescr <guilhermechaves2005@hotmail.com>
2022-06-01 16:35:18 -0300

    adicionado section projetos, botao para retornar ao topo da página

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,13 @@
+let returnButton = document.getElementById('returnBtn');
+
+function onScroll() {
+  if (scrollY > 500) {
+    returnButton.classList.remove('hideBtn');
+  } else {
+    returnButton.classList.add('hideBtn');
+  }
+}
+
 ScrollReveal({
   origin: 'left',
   distance: '30px',
@@ -6,4 +16,6 @@ ScrollReveal({
   #header,
   #about .about-info,
   #skills .skills-items,
-  #skills .skills-items .skill`);
+  #skills .skills-items .skill,
+  #projects .projects-items,
+  #projects .projects-items .project`);
