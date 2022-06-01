f332fc930a32a4915842382ae54cc433a8daa423
guilhermescr <guilhermechaves2005@hotmail.com>
2022-06-01 14:20:18 -0300

    header-presentation quase pronto, scrollbar personalizado pronto, section about me iniciada, section skills pronta temporariamente

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+  origin: 'left',
+  distance: '30px',
+  duration: 1000,
+}).reveal(`
+  #header,
+  #about .about-info,
+  #skills .skills-items,
+  #skills .skills-items .skill`);
