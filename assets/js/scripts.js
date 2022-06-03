a30ff712a4dda10ee69f61f9750a7090962f2647
guilhermescr <guilhermechaves2005@hotmail.com>
2022-06-03 17:59:00 -0300

    Revert "adiciona social bar, corrige hover de cada project"
    This reverts commit 036eac37879c3dafca01293a584076b76fb9df51.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,3 @@
-// Hide and Show Icons
-let social_icons = document.getElementById("icons");
-let instagramIcon = document.getElementById("social-i-1");
-let githubIcon = document.getElementById("social-i-2");
-let linkedinIcon = document.getElementById("social-i-3");
-
-social_icons.addEventListener("mouseenter", showIcons);
-social_icons.addEventListener("mouseleave", hideIcons);
-
-function showIcons() {
-  instagramIcon.style.display = "block";
-  githubIcon.style.display = "block";
-  linkedinIcon.style.display = "block";
-}
-
-function hideIcons() {
-  instagramIcon.style.display = "none";
-  githubIcon.style.display = "none";
-  linkedinIcon.style.display = "none";
-}
-
-// Button to the top of the page
 let returnButton = document.getElementById('returnBtn');
 
 function onScroll() {
@@ -28,21 +6,12 @@ function onScroll() {
   } else {
     returnButton.classList.add('hideBtn');
   }
-
-  // Social Bar
-  let socialBar = document.getElementById("social_bar");
-  if (scrollY >= 400) {
-    socialBar.style.display = "none";
-  } else {
-    socialBar.style.display = "block";
-  }
 }
 
-// ScrollReveal API
 ScrollReveal({
   origin: 'left',
   distance: '30px',
-  duration: 700,
+  duration: 1000,
 }).reveal(`
   #header,
   #about .about-info,
